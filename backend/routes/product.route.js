1af56c57957a9b058d7d553faac47d3a86ed6d25
siddharth2440 <siddharth2440.live@gmail.com>
2024-09-15 19:01:49 +0530

    product-routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,4 +1,15 @@
 import { Router } from "express";
+import { adminRoute, protectedRoute } from "../middleware/auth.middleware";
+import { createProduct, getAllProducts, getFeaturedProducts, getProductsByategory, getRecommendedProduct, toggleFeaturedProduct } from "../controller/product.controller";
 const router = Router();
+
+router.get("/",protectedRoute,adminRoute,getAllProducts);
+router.get("/featured-products",getFeaturedProducts);
+router.post('/',protectedRoute,adminRoute,createProduct);
+router.delete('/:id',protectedRoute,adminRoute,deleteProduct);
+router.get("/recommendations",getRecommendedProduct);
+router.get("/category/:category",getProductsByategory);
+router.patch('/:id',protectedRoute,adminRoute,toggleFeaturedProduct);
+
 
 export default router;
